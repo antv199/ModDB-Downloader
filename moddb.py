25ac3d1bd5ad45c632ac131cb42a0f2bd5f53a5a
antv199 <25984671+antv199@users.noreply.github.com>
2020-04-25 23:37:24 +0300

    Update moddb.py

--- moddb.py
+++ moddb.py
@@ -1,10 +1,11 @@
-import requests, urllib3, ping, socket, os
+import requests, urllib3, socket, os
 from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen as requestURL
 
 #modURL = str(input("Enter the ModDB mod URL or ID here: "))
 modURL = "https://www.moddb.com/downloads/start/18631/all"
-
+servers =[]
+downloadNums=[]
 #disables warnings for insecure connections
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
@@ -12,12 +13,17 @@ client = requestURL(modURL)
 raw_html = client.read()
 parsed_html = soup(raw_html, 'html.parser')
 rows = (parsed_html.findAll("div", {"class":"row"}))
+downloads = soup.find_all('span', {'class' : 'subheading'})
 
-#print(container)
+print(rows)
 
 for container in rows:
-    ServerName = container.div.p.Text
     DownloadLink = "https://moddb.com" + container.div.p.a["href"]
-    print(ServerName,DownloadLink)
+    servers.append(DownloadLink)
+    print(DownloadLink)
+
+for download in downloads:
+    downloadNums.append(download.Text)
+    print(download)
 
 client.close()
